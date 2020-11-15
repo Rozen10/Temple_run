08746b7e9ee44bfac5aee93303a39a846a8da60a
ThomasB <th.bonnerot@gmail.com>
2020-11-15 15:07:45 +0100

    first

---
+++ temple_run/sans-titre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile" tilewidth="8" tileheight="8" tilecount="2496" columns="48">
+ <image source="freetileset/svg/Tile.png" width="384" height="423"/>
+</tileset>
