a34beca3b4b100dafc740f26e195417a1741df1a
ThomasB <th.bonnerot@gmail.com>
2020-11-15 21:41:19 +0100

    delete

--- temple_run/ress.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tile" tilewidth="8" tileheight="8" tilecount="2496" columns="48">
- <image source="freetileset/svg/Tile.svg" width="384" height="423"/>
-</tileset>
