b3d926b1951acc2a569f169b07143b57408c527a
ThomasB <th.bonnerot@gmail.com>
2020-11-15 18:22:08 +0100

    delete

--- temple_run/sans2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tile" tilewidth="4" tileheight="4" tilecount="10080" columns="96">
- <image source="freetileset/svg/Tile.png" width="384" height="423"/>
-</tileset>
